30fb0fe3cfb43d1b6302689cd020423fa898bfca
“incora-dev” <dev@incorainc.com>
2022-01-03 15:52:33 +0200

    [ADD] Added decoration elements in 'Services' block

--- components/homePage/services/services.component.tsx
+++ components/homePage/services/services.component.tsx
@@ -1,22 +1,84 @@
-import {Component, Position, Div, Dv, H2, Label, Labels, P} from "./styles";
+import {
+  Component,
+  Position,
+  Div,
+  PositionTextBlock,
+  H2,
+  Label,
+  Labels,
+  P,
+  Loading,
+  PositionHexagonOne,
+  HexagonBordered,
+  HexagonFilled,
+  PositionHexagonTwo,
+  PositionHexagonThree,
+  PositionHexagonFour,
+  PositionHexagonFive,
+  PositionHexagonSix,
+  PositionHexagonSeven,
+  PositionHexagonEight,
+  PositionHexagonNine
+} from "./styles";
 import { IServices } from "@interfaces";
 
 function ServicesComponent(props: IServices) {
   const { title, text, labels } = props;
+
   return (
     <Component>
       <Position>
         <Div>
-          <Dv>
-            <H2>{title}</H2>
-            <P>{text}</P>
+          <PositionTextBlock>
+            <Div>
+              <H2>{title}</H2>
+              <P>{text}</P>
 
-            <Labels>
-              {labels.map((label, index) => <Label key={index}>{label}</Label>)}
-            </Labels>
-          </Dv>
+              <Labels>
+                {labels.map((label, index) => <Label key={index}>{label}</Label>)}
+              </Labels>
+            </Div>
+          </PositionTextBlock>
         </Div>
       </Position>
+
+      <Loading/>
+
+      <PositionHexagonOne>
+        <HexagonBordered/>
+      </PositionHexagonOne>
+
+      <PositionHexagonTwo>
+        <HexagonFilled/>
+      </PositionHexagonTwo>
+
+      <PositionHexagonThree>
+        <HexagonBordered/>
+      </PositionHexagonThree>
+
+      <PositionHexagonFour>
+        <HexagonFilled/>
+      </PositionHexagonFour>
+
+      <PositionHexagonFive>
+        <HexagonBordered/>
+      </PositionHexagonFive>
+
+      <PositionHexagonSix>
+        <HexagonBordered/>
+      </PositionHexagonSix>
+
+      <PositionHexagonSeven>
+        <HexagonBordered/>
+      </PositionHexagonSeven>
+
+      <PositionHexagonEight>
+        <HexagonFilled/>
+      </PositionHexagonEight>
+
+      <PositionHexagonNine>
+        <HexagonBordered/>
+      </PositionHexagonNine>
     </Component>
   )
 }

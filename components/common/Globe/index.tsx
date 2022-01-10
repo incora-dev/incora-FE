9068dfd0585f3a1df40f6d1ee7a29e981ba2be2d
dev@incorainc.com <dev@incorainc.com>
2022-01-10 15:21:35 +0200

    points spheres

--- components/common/Globe/index.tsx
+++ components/common/Globe/index.tsx
@@ -1,7 +1,7 @@
 import { useEffect, useRef, useState } from "react";
 import { GlobeWrapper, PointLabel } from "./style";
 import EarthTexture from "../../../public/images/globe pattern yellow 8x 9.0.png";
-import { Mesh, SphereBufferGeometry, MeshStandardMaterial } from "three";
+import THREE, { Mesh, MeshLambertMaterial, SphereBufferGeometry } from "three";
 
 let GlobeGl: Function = () => null;
 if (typeof window !== "undefined") GlobeGl = require("react-globe.gl").default;
@@ -11,18 +11,23 @@ const points = [
     lat: 5,
     lng: 13,
     size: 0.06,
+    radius: 1,
   },
   {
     lat: 5,
     lng: 1,
     size: 0.03,
+    radius: 0.6,
   },
   {
     lat: 15,
     lng: -5,
     size: 0.03,
+    radius: 0.6,
   },
 ];
+
+type Point = typeof points[0];
 
 const Globe = () => {
   const globeEl: any = useRef();
@@ -30,9 +35,6 @@ const Globe = () => {
   const altitude = 1.4;
 
   useEffect(() => {
-    console.log("globe.current", globeEl.current);
-    console.log("points", points);
-
     const { lat, lng } = points[0];
 
     globeEl.current.pointOfView({
@@ -43,39 +45,36 @@ const Globe = () => {
     globeEl.current.controls().enableZoom = false;
   }, []);
 
-  // const createSphere = (point: typeof pointsSpheres[0]) => {
-  //   const sphere = new Mesh(
-  //     new SphereBufferGeometry(point.radius),
-  //     new MeshStandardMaterial({ color: "white" })
-  //   );
+  const pointsSpheres = (point: Point) => {
+    const coords = globeEl.current.getCoords(point.lat, point.lng, point.size);
 
-  //   sphere.position.x = point.lat;
-  //   sphere.position.y = point.lng;
+    const sphere = new Mesh(
+      new SphereBufferGeometry(point.radius),
+      new MeshLambertMaterial({ color: "white" })
+    );
+    sphere.position.x = coords.x;
+    sphere.position.y = coords.y;
+    sphere.position.z = coords.z;
 
-  //   return sphere;
-  // };
-
-  const pointsLabels = points.map((point) => {
-    return {
-      ...point,
-      radius: 3,
-    };
-  });
+    return sphere;
+  };
 
   return (
     <GlobeWrapper>
-        <GlobeGl
-          width={1366}
-          height={1200}
-          ref={globeEl}
-          globeImageUrl={EarthTexture.src}
-          backgroundColor="#18181A"
-          showAtmosphere={false}
-          pointsData={points}
-          pointAltitude="size"
-          pointColor={() => "white"}
-          pointRadius={0.1}
-        />
+      <GlobeGl
+        width={1366}
+        height={1200}
+        ref={globeEl}
+        globeImageUrl={EarthTexture.src}
+        backgroundColor="#18181A"
+        showAtmosphere={false}
+        pointsData={points}
+        pointAltitude="size"
+        pointColor={() => "white"}
+        pointRadius={0.1}
+        customLayerData={points}
+        customThreeObject={pointsSpheres}
+      />
     </GlobeWrapper>
   );
 };

b2b8739fc1ce73a02812c4375ca82b103ddfd73f
dev@incorainc.com <dev@incorainc.com>
2022-01-28 20:13:57 +0200

    start making pagination

--- components/Pagination/index.tsx
+++ components/Pagination/index.tsx
@@ -6,7 +6,7 @@ import {
   LeftArrow,
   RightArrow,
 } from "./Pagination.style";
-import { useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 import { maxSlideNumber, minSlideNumber } from "../../utils";
 
 const numbers = ["1", "2", "3", "...", "8", "9", "10"];
@@ -15,12 +15,12 @@ interface IPagination {
   totalCount: number;
   currentPage: number;
   pageSize: number;
-  onPageChanged: (value: number) => void;
+  onPageChanged: (page: number) => void;
   siblingCount?: number;
 }
 
 const getNUmberButtons = (
-  numbers: string[],
+  numbers: (string | number)[],
   select: number,
   setSelect: Function
 ) => {
@@ -43,6 +43,71 @@ const getNUmberButtons = (
   );
 };
 
+interface IUsePagination {
+  totalCount: number;
+  currentPage: number;
+  pageSize: number;
+  siblingCount?: number;
+}
+
+export const usePagination = ({
+  totalCount,
+  pageSize,
+  siblingCount = 1,
+  currentPage,
+}: IUsePagination) => {
+  const paginationRange = useMemo(() => {
+    const totalPageCount = Math.ceil(totalCount / pageSize);
+
+    const range = (start: number, end: number) => {
+      let length = end - start + 1;
+
+      return Array.from({ length }, (_, idx) => idx + start);
+    };
+
+    const totalPageNumbers = siblingCount + 5;
+
+    if (totalPageNumbers >= totalPageCount) {
+      return range(1, totalPageCount);
+    }
+
+    const leftSiblingIndex = Math.max(currentPage - siblingCount, 1);
+    const rightSiblingIndex = Math.min(
+      currentPage + siblingCount,
+      totalPageCount
+    );
+
+    const shouldShowLeftDots = leftSiblingIndex > 2;
+    const shouldShowRightDots = rightSiblingIndex < totalPageCount - 2;
+
+    const firstPageIndex = 1;
+    const lastPageIndex = totalPageCount;
+
+    if (!shouldShowLeftDots && shouldShowRightDots) {
+      let leftItemCount = 3 + 2 * siblingCount;
+      let leftRange = range(1, leftItemCount);
+
+      return [...leftRange, "...", totalPageCount];
+    }
+
+    if (shouldShowLeftDots && !shouldShowRightDots) {
+      let rightItemCount = 3 + 2 * siblingCount;
+      let rightRange = range(
+        totalPageCount - rightItemCount + 1,
+        totalPageCount
+      );
+      return [firstPageIndex, "...", ...rightRange];
+    }
+
+    if (shouldShowLeftDots && shouldShowRightDots) {
+      let middleRange = range(leftSiblingIndex, rightSiblingIndex);
+      return [firstPageIndex, "...", ...middleRange, "...", lastPageIndex];
+    }
+  }, [totalCount, pageSize, siblingCount, currentPage]);
+
+  return paginationRange;
+};
+
 const Pagination = ({
   totalCount,
   currentPage,
@@ -51,27 +116,53 @@ const Pagination = ({
   siblingCount,
 }: IPagination) => {
   const [select, setSelect] = useState(0);
-  const totalPageCount = Math.ceil(totalCount / pageSize);
+
+  const paginationRange = usePagination({
+    totalCount,
+    currentPage,
+    pageSize,
+    siblingCount,
+  });
+
+  const onNext = () => {
+    onPageChanged(currentPage + 1);
+  };
+
+  const onPrevious = () => {
+    onPageChanged(currentPage - 1);
+  };
+
+  const lastPage =
+    paginationRange && paginationRange[paginationRange.length - 1];
+
+  useEffect(() => {
+    console.log(paginationRange);
+  });
 
   return (
-    <Div>
-      <NumberBlock>
-        <LeftArrow
-          changeColor={select === 0}
-          onClick={() => setSelect(maxSlideNumber(select))}
-        >
-          <Arrow />
-        </LeftArrow>
-        {getNUmberButtons(numbers, select, setSelect)}
-        <RightArrow
-          changeColor={select === numbers.length - 1}
-          onClick={() => setSelect(minSlideNumber(select, numbers.length))}
-        >
-          <Arrow />
-        </RightArrow>
-      </NumberBlock>
-    </Div>
+    <>
+      {paginationRange && (
+        <Div>
+          <NumberBlock>
+            <LeftArrow changeColor={select === 0} onClick={onPrevious}>
+              <Arrow />
+            </LeftArrow>
+            {getNUmberButtons(paginationRange, select, setSelect)}
+            <RightArrow
+              changeColor={select === numbers.length - 1}
+              onClick={onNext}
+            >
+              <Arrow />
+            </RightArrow>
+          </NumberBlock>
+        </Div>
+      )}
+    </>
   );
 };
 
 export default Pagination;
+
+// () => setSelect(maxSlideNumber(select))
+
+// () => setSelect(maxSlideNumber(select))

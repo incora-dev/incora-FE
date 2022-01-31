e92c9606ef4ed04526a77135d0ba4d5310226aa5
dev-incora-ui <dev@incorainc.com>
2022-01-31 19:07:50 +0200

    industry page fixes

--- components/BenefitsAndSolutions/style.ts
+++ components/BenefitsAndSolutions/style.ts
@@ -24,12 +24,12 @@ export const BenefitsAndSolutionsWrap = styled.div`
   background-color: ${({ bgColor }: IBackground) => bgColor};
 
   @media only screen and (max-width: ${themeGet('breakpoints.mobile')}) {
-    height: 210vh;
+    height: 220vh;
   }
-  @media only screen and (max-width: ${theme.breakpoints.mMobile}) {
+  @media only screen and (max-width:  ${themeGet('breakpoints.mMobile')}) {
     height: 260vh;
   }
-  @media only screen and (max-width: ${theme.breakpoints.xsMobile}) {
+  @media only screen and (max-width:  ${themeGet('breakpoints.xsMobile')}) {
     height: 350vh;
   }
 `;

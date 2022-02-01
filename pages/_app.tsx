6b390cec2832dddb28ed2b5cf07935bb05888480
dev-incora-ui <dev@incorainc.com>
2022-02-01 14:21:28 +0200

    lock orientation

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,6 +7,7 @@ import store from "../services/store";
 import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
 import { GRAPH_QL_LINK } from "../constants";
 import NavState from "../services/context/mainMenu";
+import { useEffect } from "react";
 
 const client = new ApolloClient({
   uri: GRAPH_QL_LINK,
@@ -14,6 +15,9 @@ const client = new ApolloClient({
 });
 
 function MyApp({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    screen.orientation.lock("portrait-primary");
+  }, [])
   return (
     <>
     <NavState>

b9036204d187a4b063ff43ef446bd0b5e42f6d95
incora-dev <dev@incorainc.com>
2022-02-08 16:22:44 +0200

    Added google analytics script

--- pages/_document.js
+++ pages/_document.js
@@ -40,6 +40,17 @@ class MyDocument extends Document {
           <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
           <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;400;500;600;700;800;900&display=swap" rel="stylesheet"/>
           <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100&display=swap" rel="stylesheet"/>
+          <script async src="https://www.googletagmanager.com/gtag/js?id=UA-73161009-1"></script>
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+                gtag('config', 'UA-73161009-1');
+              `,
+            }}
+          />
         </Head>
         <body>
           <Main/>

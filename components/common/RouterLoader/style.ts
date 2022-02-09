09e672583a8f49e7dcff02a91ddfd4d9c86e5f9a
incora-dev <dev@incorainc.com>
2022-02-09 16:43:25 +0200

    Fixed loader

--- components/common/RouterLoader/style.ts
+++ components/common/RouterLoader/style.ts
@@ -2,6 +2,11 @@ import themeGet from "@styled-system/theme-get";
 import styled from "styled-components";
 
 export const RouterLoaderWrapper = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
   width: 100%;
   height: 100vh;
   display: flex;
@@ -9,8 +14,7 @@ export const RouterLoaderWrapper = styled.div`
   align-items: center;
   background-color: ${themeGet("colors.white")};
   transition: all 0.7s ease-in-out;
-  z-index: 10;
-  position: sticky;
+  z-index: 100;
 
   svg {
     margin-right: 5px;

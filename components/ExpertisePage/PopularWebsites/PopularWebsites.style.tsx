2f0f311f9e91ec6f3f09855d96199d353c7e097b
dev@incorainc.com <dev@incorainc.com>
2022-02-11 19:14:42 +0200

    aligns of popular website on tablet

--- components/ExpertisePage/PopularWebsites/PopularWebsites.style.tsx
+++ components/ExpertisePage/PopularWebsites/PopularWebsites.style.tsx
@@ -10,12 +10,12 @@ export const Wrapper = styled.div`
   margin: 0 auto;
   padding: 120px 0;
 
-  @media only screen and (max-width: ${themeGet('breakpoints.tabletPro')}) {
+  @media only screen and (max-width: ${themeGet("breakpoints.tabletPro")}) {
     max-width: 100%;
     padding: 70px 25px;
   }
 
-  @media only screen and (max-width: ${themeGet('breakpoints.mobile')}) {
+  @media only screen and (max-width: ${themeGet("breakpoints.mobile")}) {
     padding: 50px 15px;
   }
 `;
@@ -23,14 +23,14 @@ export const Wrapper = styled.div`
 export const TextBlock = styled.div`
   display: flex;
   flex-direction: column;
-  
+
   row-gap: 90px;
 
-  @media only screen and (max-width: ${themeGet('breakpoints.tabletPro')}) {
+  @media only screen and (max-width: ${themeGet("breakpoints.tabletPro")}) {
     row-gap: 70px;
   }
 
-  @media only screen and (max-width: ${themeGet('breakpoints.sTablet')}) {
+  @media only screen and (max-width: ${themeGet("breakpoints.sTablet")}) {
     row-gap: 20px;
   }
 `;
@@ -41,22 +41,21 @@ export const H2 = styled.h2`
   font-size: var(--fs-36);
   line-height: 54px;
 
-
   letter-spacing: 0.05em;
-  
+
   color: ${themeGet("colors.black")};
 
-  @media only screen and (max-width: ${themeGet('breakpoints.tabletPro')}) {
+  @media only screen and (max-width: ${themeGet("breakpoints.tabletPro")}) {
     font-size: var(--fs-36);
     line-height: 50px;
   }
 
-  @media only screen and (max-width: ${themeGet('breakpoints.sTablet')}) {
+  @media only screen and (max-width: ${themeGet("breakpoints.sTablet")}) {
     font-size: var(--fs-28);
     line-height: 42px;
   }
 
-  @media only screen and (max-width: ${themeGet('breakpoints.mobile')}) {
+  @media only screen and (max-width: ${themeGet("breakpoints.mobile")}) {
     line-height: 40px;
   }
 `;
@@ -69,36 +68,32 @@ export const IconsBlock = styled.div`
   align-items: center;
   justify-items: center;
 
-  @media only screen and (max-width: ${themeGet('breakpoints.tablet')}) {
+  @media only screen and (max-width: ${themeGet("breakpoints.tablet")}) {
     column-gap: 45px;
   }
 
-  @media only screen and (max-width: ${themeGet('breakpoints.iPadAir')}) {
+  @media only screen and (max-width: ${themeGet("breakpoints.iPadAir")}) {
     display: flex;
     flex-wrap: wrap;
     column-gap: 50px;
   }
 
-  @media only screen and (max-width: ${themeGet('breakpoints.iPadMini')}) {
-    column-gap: 107px;
-  }
-
-  @media only screen and (max-width: ${themeGet('breakpoints.sTablet')}) {
+  @media only screen and (max-width: ${themeGet("breakpoints.sTablet")}) {
     display: block;
     text-align: left;
     margin: 0 -15px;
   }
 
   > span {
-    @media only screen and (max-width: ${themeGet('breakpoints.sTablet')}) {
+    @media only screen and (max-width: ${themeGet("breakpoints.sTablet")}) {
       margin: 15px !important;
     }
 
-    @media only screen and (max-width: ${themeGet('breakpoints.mobile')}) {
+    @media only screen and (max-width: ${themeGet("breakpoints.mobile")}) {
       margin: 5px 15px !important;
     }
   }
-  
+
   svg {
     cursor: pointer;
   }
